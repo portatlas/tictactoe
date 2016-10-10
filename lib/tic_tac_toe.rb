b08de6c9a74ce03932f52b76069b64279106a6ba
portatlas <a.vathanaka@gmail.com>
2016-10-10 10:38:44 -0700

    Create a constant WIN_COMBOS as a nested array of all the possible ways to win in TTT

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,6 +2,10 @@ class TicTacToe
 
   attr_accessor :board
 
+  WIN_COMBOS = [[0,1,2],[3,4,5],[6,7,8],
+                [0,3,6],[1,4,7],[2,5,8],
+                [0,4,8],[2,4,6]]
+
   def initialize (turn = "X")
     @board = Array.new(9, " ")
     @turn = turn
