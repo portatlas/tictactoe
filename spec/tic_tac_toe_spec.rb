d33c2c0f5ea7947e041819f29076c58692428897
portatlas <a.vathanaka@gmail.com>
2016-10-10 08:29:23 -0700

    Remove lingering comma in the array

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -4,7 +4,7 @@ describe TicTacToe do
   describe '#initialize' do
     it 'creates a empty board represented by an array of 9 slots' do
       ttt = TicTacToe.new
-      expect(ttt.instance_variable_get(:@board)).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " ", ])
+      expect(ttt.instance_variable_get(:@board)).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " "])
     end
   end
 end
