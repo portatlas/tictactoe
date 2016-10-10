987e589cac20db76c4b50fce25426484451872fe
portatlas <a.vathanaka@gmail.com>
2016-10-10 08:45:33 -0700

    Refactor test to reduce instantitiating a new TicTacToe for each test

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../lib/tic_tac_toe.rb'
 
 describe TicTacToe do
+  let(:ttt){TicTacToe.new}
+
   describe '#initialize' do
     it 'creates a empty board represented by an array of 9 slots' do
-      ttt = TicTacToe.new
-      expect(ttt.instance_variable_get(:@board)).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+      expect(ttt.board).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " "])
     end
   end
+
 end
