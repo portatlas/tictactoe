1ceaba0ec1a036191a43dec9f78d110f3f1cb267
portatlas <a.vathanaka@gmail.com>
2016-10-10 16:16:50 -0700

    Update how the board is display by adding grid lines

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,9 +14,11 @@ class TicTacToe
   end
 
   def show_board
-    puts " #{@board[0]} " + " #{@board[1]} " + " #{@board[2]} "
-    puts " #{@board[3]} " + " #{@board[4]} " + " #{@board[5]} "
-    puts " #{@board[6]} " + " #{@board[7]} " + " #{@board[8]} "
+    puts " #{@board[0]} " + "|" + " #{@board[1]} " + "|" + " #{@board[2]} "
+    puts "- - - - - -"
+    puts " #{@board[3]} " + "|" + " #{@board[4]} " + "|" + " #{@board[5]} "
+    puts "- - - - - -"
+    puts " #{@board[6]} " + "|" + " #{@board[7]} " + "|" + " #{@board[8]} "
   end
 
   def move(input)
@@ -76,3 +78,6 @@ class TicTacToe
   end
 
 end
+
+ttt = TicTacToe.new
+ttt.show_board
