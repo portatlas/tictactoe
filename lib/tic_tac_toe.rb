69031896d2710a49d55737dbf6189b9a7b36179e
Andrew Vathanakamsang <a.vathanaka@gmail.com>
2016-10-10 21:31:48 -0700

    Revert "Play ttt game"

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class TicTacToe
 
-  attr_accessor :board, :turn, :move
+  attr_accessor :board, :turn
 
   WIN_COMBOS = [[0,1,2],[3,4,5],[6,7,8],
                 [0,3,6],[1,4,7],[2,5,8],
@@ -11,37 +11,21 @@ class TicTacToe
   def initialize (board = Array.new(9, " "), turn = "X")
     @board = board
     @turn = turn
-    @movelist = Array.new
   end
 
   def show_board
-    puts " #{@board[0]} " + "|" + " #{@board[1]} " + "|" + " #{@board[2]} "
-    puts "- - - - - -"
-    puts " #{@board[3]} " + "|" + " #{@board[4]} " + "|" + " #{@board[5]} "
-    puts "- - - - - -"
-    puts " #{@board[6]} " + "|" + " #{@board[7]} " + "|" + " #{@board[8]} "
+    puts " #{@board[0]} " + " #{@board[1]} " + " #{@board[2]} "
+    puts " #{@board[3]} " + " #{@board[4]} " + " #{@board[5]} "
+    puts " #{@board[6]} " + " #{@board[7]} " + " #{@board[8]} "
   end
 
   def move(input)
-    @board[input] = @turn
-    @turn = whose_turn
-    self
+    if valid_move?(input)
+      played_move = TicTacToe.new(@board.dup, whose_turn("O", "X"))
+      played_move.board[input] = turn
+      played_move
+    end
   end
-
-  def unmove
-    binding.pry
-    @board[@movelist.pop] = " "
-    @turn = whose_turn
-    self
-  end
-
-  # def move(input)
-  #   if valid_move?(input)
-  #     played_move = TicTacToe.new(@board.dup, whose_turn("O", "X"))
-  #     played_move.board[input] = turn
-  #     played_move
-  #   end
-  # end
 
   def valid_slots
     @board.each_with_index.map{|symbol, index| index if symbol==" "}.reject{|slots| slots == nil }
@@ -51,8 +35,8 @@ class TicTacToe
     valid_slots.include?(input)
   end
 
-  def whose_turn
-    turn == "X" ? "O" : "X"
+  def whose_turn(x, o)
+    turn == "X" ? x : o
   end
 
   def won?
@@ -79,77 +63,16 @@ class TicTacToe
     end
   end
 
-  def check_branch
-    return 100 if winner == "X"
-    return -100 if winner == "O"
+  def minimax(increment = 10)
+    return 1000 if winner == "X"
+    return -1000 if winner == "O"
     return 0 if draw?
-  end
 
-  def minimax(index = nil)
-    move(index) if index
-
-    branch_value = check_branch
-    return branch_value if check_branch
-
-    # valid_slots.map{ |index| move(index).minimax(increment + 10) }.send(whose_turn(:max, :min)) + whose_turn(- increment, increment)
-    valid_slots.map { |index| minimax(index).send(@turn == "X" ? :- : :+, @movelist.count+1)}.send(@turn == "X" ? :max : :min)
+    valid_slots.map{ |index| move(index).minimax(increment + 10) }.send(whose_turn(:max, :min)) + whose_turn(- increment, increment)
   end
 
   def optimal_move
-    valid_slots.send(@turn == "X" ? :max_by : :min_by){|index| move(index).minimax}
+    valid_slots.send(whose_turn(:max_by, :min_by)){|index| move(index).minimax}
   end
 
-end
-
-class Game
-
-  # def ask_for_player
-  #   puts "Who do you want to play first?"
-  #   puts "1. human"
-  #   puts "2. computer"
-  #   while true
-  #     print "choice: "
-  #     ans = gets.chomp
-  #     return "human"    if ans == "1"
-  #     return "computer" if ans == "2"
-  #   end
-  # end
-
-  def get_user_input
-    puts "Enter the slot you want"
-    user_input = gets.chomp
-    return user_input.to_i
-  end
-
-  def other_player
-    @player == "human" ? "computer" : "human"
-  end
-
-  def play
-    # @player = ask_for_player
-    puts "Lets Play Tic Tac Toe!"
-    ttt = TicTacToe.new
-    ttt.move(7)
-    puts ttt.show_board
-      # binding.pry
-    # while ttt.game_over?
-
-      index = get_user_input
-      ttt.move(0)
-      ttt.show_board
-      @player = other_player
-
-    # end
-    # if ttt.draw?
-    #   puts "It is a draw"
-    # else
-    #   puts "Winner"
-    # end
-  end
-
-
-end
-
-if __FILE__ == $0
-  Game.new.play
 end
--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -5,33 +5,33 @@ describe TicTacToe do
 
   describe '#minimax' do
     context 'a player wins' do
-      it 'returns 100 if it is a winning board is X' do
+      it 'returns 1000 if it is a winning board is X' do
         board = ["X", "X", "X", "O", "X", "X", "O", "X", "O"]
-        expect(TicTacToe.new(board, "X").check_branch).to eq 100
+        expect(TicTacToe.new(board, "X").minimax).to eq 1000
       end
 
-      it 'returns -100 if it is a winning board is O' do
+      it 'returns -1000 if it is a winning board is O' do
         board = ["X", "X", "O", "O", "X", "X", "O", "O", "O"]
-        expect(TicTacToe.new(board, "O").check_branch).to eq -100
+        expect(TicTacToe.new(board, "O").minimax).to eq -1000
       end
     end
 
     context 'draw' do
       it 'returns 0 if it is a draw' do
         board = ["X", "O", "X", "X", "O", "O", "O", "X", "O"]
-        expect(TicTacToe.new(board, "O").check_branch).to eq 0
+        expect(TicTacToe.new(board, "O").minimax).to eq 0
       end
     end
 
     context 'one move away from win' do
-      it 'returns 99 if it is a winning board is X' do
+      it 'returns 990 if it is a winning board is X' do
         board = ["X", "X", " ", " ", " ", " ", " ", " ", " "]
-        expect(TicTacToe.new(board, "X").minimax).to eq 99
+        expect(TicTacToe.new(board, "X").minimax).to eq 990
       end
 
-      it 'returns -99 if it is a winning board is O' do
+      it 'returns -990 if it is a winning board is O' do
         board = [" ", " ", " ", " ", " ", " ", " ", "O", "O"]
-        expect(TicTacToe.new(board, "O").minimax).to eq -99
+        expect(TicTacToe.new(board, "O").minimax).to eq -990
       end
     end
   end
