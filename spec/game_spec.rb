0016e774b683fd050918d1d47a01340249988772
portatlas <a.vathanaka@gmail.com>
2016-10-17 14:31:47 -0700

    Fix typo *inpt to input

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,7 +13,7 @@ describe Game do
 
   describe  '#prompt_user_for_input' do
     it 'ask users to input the position on available spots where they want to place an X' do
-      expect(new_game.prompt_user_for_inpt).to include("Enter a number 0, 1, 2, 3, 4, 5, 6, 7, 8 to place an X")
+      expect(new_game.prompt_user_for_input).to include("Enter a number 0, 1, 2, 3, 4, 5, 6, 7, 8 to place an X")
     end
   end
 
