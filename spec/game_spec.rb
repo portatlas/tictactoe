9e7588b8f4f33bdd8a208939fcb356e3140cea0f
portatlas <a.vathanaka@gmail.com>
2016-10-17 14:07:49 -0700

    Test that Game initializes with a instance of TicTacToe

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,14 @@
 require './lib/game.rb'
+require './lib/tic_tac_toe.rb'
 
 describe Game do
+  let(:ttt){TicTacToe.new}
+  let(:new_game){Game.new(ttt)}
+
+  describe '#initialize' do
+    it 'with a new instance of TicTacToe' do
+      expect(new_game.ttt).to eq(ttt)
+    end
+  end
+
 end
