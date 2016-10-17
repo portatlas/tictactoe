6a7ec0793140c64edd84723f5e23b9044ed20dc3
portatlas <a.vathanaka@gmail.com>
2016-10-17 15:14:11 -0700

    Create test for #get_user_input to take in an string input and convert it into a integer

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,4 @@
+require 'stringio'
 require './lib/game.rb'
 require './lib/tic_tac_toe.rb'
 
@@ -11,9 +12,16 @@ describe Game do
     end
   end
 
-  describe  '#prompt_user_for_input' do
+  describe '#prompt_user_for_input' do
     it 'ask users to input the position on all available slots where they want to place an X' do
       expect(new_game.prompt_user_for_input).to eq("Enter a number 0, 1, 2, 3, 4, 5, 6, 7, 8 to place an X")
+    end
+  end
+
+  describe '#get_user_input' do
+    it 'takes in a user input and returns an integer' do
+      allow(new_game).to receive(:gets).and_return("2")
+      expect(new_game.get_user_input).to eq(2)
     end
   end
 
