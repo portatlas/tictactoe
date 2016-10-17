01deb4f51e74ed068cf8089fa677500dd6b3cbd0
portatlas <a.vathanaka@gmail.com>
2016-10-17 14:41:51 -0700

    Update spot to slot to be consistent with other test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,8 +12,8 @@ describe Game do
   end
 
   describe  '#prompt_user_for_input' do
-    it 'ask users to input the position on available spots where they want to place an X' do
-      expect(new_game.prompt_user_for_input).to include("Enter a number 0, 1, 2, 3, 4, 5, 6, 7, 8 to place an X")
+    it 'ask users to input the position on all available slots where they want to place an X' do
+      expect(new_game.prompt_user_for_input).to eq("Enter a number 0, 1, 2, 3, 4, 5, 6, 7, 8 to place an X")
     end
   end
 
