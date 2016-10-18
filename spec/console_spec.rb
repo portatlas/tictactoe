c17ade21cf162918c6fc2097147156c930955cd1
portatlas <a.vathanaka@gmail.com>
2016-10-18 08:58:00 -0700

    Comment out test for now as it is not working

--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -4,22 +4,25 @@ require './lib/ui.rb'
 require './lib/tictactoe.rb'
 
 describe Console do
+
   let(:gametype){TicTacToe.new}
   let(:io){Console.new}
   let(:ui){Ui.new(io)}
   let(:new_game){Game.new({gametype: gametype, ui: ui})}
 
-  # describe '#prompt_user_for_input' do
-  #   it 'ask users to input the position on all available slots where they want to place an X' do
-  #     expect(io.prompt_user_for_input(gametype)).to eq("Enter a number 0, 1, 2, 3, 4, 5, 6, 7, 8 to place an X")
-  #   end
-  # end
 
-  # describe '#winner' do
-  #   it 'returns You won! if the user won the game' do
-  #     new_game.gametype.won?("X")
-  #     expect(io.display_winner_message(gametype)).to eq ("You won!")
-  #   end
+    # describe '#prompt_user_for_input' do
+    #   it 'ask users to input the position on all available slots where they want to place an X' do
+    #     # STDOUT.should_receive(:puts).with("Enter a number 0, 1, 2, 3, 4, 5, 6, 7, 8 to place an X")
+    #     STDOUT.should_receive(:puts).with("Hello")
+    #   end
+    # end
+
+    # describe '#winner' do
+    #   it 'returns You won! if the user won the game' do
+    #     new_game.gametype.won?("X")
+    #     expect(io.display_winner_message(gametype)).to eq ("You won!")
+    #   end
 
     # it 'returns Computer Won if the computer won the game' do
     #   io.gametype.board = ["O", "O", "X",
