c5d7a7dc89c1ec214786061ec6f61de73e653ca9
portatlas <a.vathanaka@gmail.com>
2016-10-17 23:43:18 -0700

    Comment out code that is related to stdin and stdout in Game class

--- lib/game.rb
+++ lib/game.rb
@@ -2,24 +2,25 @@ require_relative 'tic_tac_toe'
 
 class Game
 
-  attr_accessor :gametype
+  attr_accessor :gametype, :ui
 
-  def initialize(gametype = nil)
+  def initialize(gametype = nil, ui = nil)
+    @ui = ui
     @gametype = gametype
   end
 
-  def display_intro_msg
-    puts "Welcome to #{gametype.desc[:name]} \n#{gametype.desc[:instructions]}"
-    gametype.show_board
-  end
+  # def display_intro_msg
+  #   puts "Welcome to #{gametype.desc[:name]} \n#{gametype.desc[:instructions]}"
+  #   ui.show_board(gametype.board)
+  # end
 
   def prompt_user_for_input
     "Enter a number #{@gametype.valid_slots.join(", ")} to place an X"
   end
 
-  def get_user_input
-    user_input = gets.chomp.to_i
-  end
+  # def get_user_input
+  #   user_input = gets.chomp.to_i
+  # end
 
   def winner
     if gametype.won?("X")
@@ -48,15 +49,16 @@ class Game
   def alternate_move(user_input)
     user_move(user_input)
     comp_move
-    gametype.show_board
+    ui.show_board(gametype.board)
     winner
   end
 
   def play
-    display_intro_msg
+    ui.display_intro_msg(gametype)
+    ui.show_board(gametype.board)
     while !gametype.game_over?
       puts prompt_user_for_input
-      puts alternate_move(get_user_input)
+      puts alternate_move(ui.get_user_input)
     end
   end
 
