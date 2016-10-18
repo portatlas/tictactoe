5f99c41ceaea3fc3a3b05799f3e66513a148be60
portatlas <a.vathanaka@gmail.com>
2016-10-18 02:03:31 -0700

    Remove the #show_method from the TicTacToe class as it relates to stdout

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,14 +17,6 @@ class TicTacToe
      instructions: "You and the computer will take turns placing a 'X' and 'O' respectively, the player who succeeds in placing three of their marks in a horizontal, vertical or diagonal row wins."}
   end
 
-  # def show_board
-  #   puts " #{@board[0]} " + "|" + " #{@board[1]} " + "|" + " #{@board[2]} "
-  #   puts "- - - - - -"
-  #   puts " #{@board[3]} " + "|" + " #{@board[4]} " + "|" + " #{@board[5]} "
-  #   puts "- - - - - -"
-  #   puts " #{@board[6]} " + "|" + " #{@board[7]} " + "|" + " #{@board[8]} "
-  # end
-
   def move(input)
       played_move = TicTacToe.new(@board.dup, whose_turn("O", "X"))
       played_move.board[input.to_i] = turn
