ec46d036312b87e61a89d17e2d626e2be8554abe
portatlas <a.vathanaka@gmail.com>
2016-10-18 12:15:38 -0700

    Add test for #prompt_user_for_input #display_invalid_input #winner

--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -26,7 +26,7 @@ describe Console do
   end
 
   describe '#display_intro_msg' do
-    it 'ask users to input the position on all available slots where they want to place an X' do
+    it 'puts the intro message for the game' do
       output = capture_puts{ io.display_intro_msg(gametype)}
 
       expect(output).to include("Welcome to #{gametype.desc[:name]}")
@@ -34,26 +34,54 @@ describe Console do
     end
   end
 
-  # describe '#winner' do
-  #   it 'returns You won! if the user won the game' do
-  #     new_game.gametype.won?("X")
-  #     expect(io.display_winner_message(gametype)).to eq ("You won!")
-  #   end
+  describe '#prompt_user_for_input' do
+    it 'ask users to input the position on all available slots where they want to place an X' do
+      output = capture_puts{ io.prompt_user_for_input(gametype)}
 
-    # it 'returns Computer Won if the computer won the game' do
-    #   io.gametype.board = ["O", "O", "X",
-    #                              "X", "O", "X",
-    #                              "X", "O", "O"]
-    #   expect(io.winner).to eq "Computer won!"
-    # end
+      expect(output).to include("Enter a number #{gametype.valid_slots.join(", ")} to place an X")
+    end
+  end
 
-    # it 'returns It\'s a draw! if there is no winner' do
-    #   io.gametype.board = ["X", "O", "X",
-    #                              "X", "O", "O",
-    #                              "O", "X", "O"]
-    #   expect(io.winner).to eq "It's a draw!"
-    # end
-  # end
+  describe '#display_invalid_input' do
+    it 'displays a invalid message when user inputs is invalid' do
+      output = capture_puts{ io.display_invalid_input}
+
+      expect(output).to include("Invalid input try again")
+    end
+  end
+
+
+  describe '#winner' do
+    it 'returns You won! if the user won the game' do
+      win_board = ["X", "X", "X",
+                   "O", "O", "X",
+                   "X", "O", "O"]
+      winning_game = TicTacToe.new
+      winning_game.board = win_board
+      output = capture_puts{ io.display_winner_message(winning_game)}
+      expect(output).to include ("You won!")
+    end
+
+    it 'returns Computer won! if the user won the game' do
+      comp_win_board = ["X", "O", " ",
+                        "O", "O", "X",
+                        "O", "O", "O"]
+      winning_game = TicTacToe.new
+      winning_game.board = comp_win_board
+      output = capture_puts{ io.display_winner_message(winning_game)}
+      expect(output).to include ("Computer won!")
+    end
+
+    it 'returns Computer won! if the user won the game' do
+      draw_board = ["X", "O", "O",
+                    "O", "X", "X",
+                    "X", "O", "O"]
+      winning_game = TicTacToe.new
+      winning_game.board = draw_board
+      output = capture_puts{ io.display_winner_message(winning_game)}
+      expect(output).to include ("It's a draw!")
+    end
+  end
 
 
 end
