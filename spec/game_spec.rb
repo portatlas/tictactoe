619f7263527aff579673c522ff737de84f2b17d1
portatlas <a.vathanaka@gmail.com>
2016-10-17 17:58:23 -0700

    Add test for #winner in Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,18 +3,41 @@ require './lib/game.rb'
 require './lib/tic_tac_toe.rb'
 
 describe Game do
-  let(:ttt){TicTacToe.new}
-  let(:new_game){Game.new(ttt)}
+  let(:gametype){TicTacToe.new}
+  let(:new_game){Game.new(gametype)}
 
   describe '#initialize' do
     it 'with a new instance of TicTacToe' do
-      expect(new_game.gametype).to eq(ttt)
+      expect(new_game.gametype).to eq(gametype)
     end
   end
 
   describe '#prompt_user_for_input' do
     it 'ask users to input the position on all available slots where they want to place an X' do
       expect(new_game.prompt_user_for_input).to eq("Enter a number 0, 1, 2, 3, 4, 5, 6, 7, 8 to place an X")
+    end
+  end
+
+  describe '#winner' do
+    it 'returns You won! if the user won the game' do
+      new_game.gametype.board = ["X", "X", "X",
+                                 "O", "X", "X",
+                                 "O", "X", "O"]
+      expect(new_game.winner).to eq ("You won!")
+    end
+
+    it 'returns Computer Won if the computer won the game' do
+      new_game.gametype.board = ["O", "O", "X",
+                                 "X", "O", "X",
+                                 "X", "O", "O"]
+      expect(new_game.winner).to eq "Computer won!"
+    end
+
+    it 'returns It\'s a draw! if there is no winner' do
+      new_game.gametype.board = ["X", "O", "X",
+                                 "X", "O", "O",
+                                 "O", "X", "O"]
+      expect(new_game.winner).to eq "It's a draw!"
     end
   end
 
