010d3b70103df23eea69c13b71b4f8c75ecbc71f
portatlas <a.vathanaka@gmail.com>
2016-10-18 00:52:06 -0700

    Add #prompt_user_for_input and #display_winner_message

--- lib/ui.rb
+++ lib/ui.rb
@@ -11,12 +11,20 @@ class Ui
     io.display_intro_msg(game_being_played)
   end
 
+  def prompt_user_for_input(game_being_played)
+    io.prompt_user_for_input(game_being_played)
+  end
+
   def show_board(board)
     io.show_board(board)
   end
 
   def get_user_input
     io.get_user_input
+  end
+
+  def display_winner_message(game_being_played)
+    io.display_winner_message(game_being_played)
   end
 
 end
