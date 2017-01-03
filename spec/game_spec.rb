c42c44752ec845772b0ed21dae01ea10cd5d650c
portatlas <a.vathanaka@gmail.com>
2017-01-03 15:44:06 +0700

    Remove specs for comp move

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -23,26 +23,6 @@ describe Game do
     end
   end
 
-  describe '#comp_move' do
-    it 'returns a TicTacToe board if a move is made' do
-      played_ttt = TictactoeBoard.new
-      played_ttt.board_arr = ["X", " ", " ",
-                              " ", " ", " ",
-                              " ", " ", " "]
-      first_move_game = Game.new({gametype: played_ttt, ui: ui, rules: rules, comp_player: comp_player })
-      comp_move = first_move_game.comp_move
-      expect(comp_move).to be_an_instance_of(TictactoeBoard)
-    end
-
-    it 'returns nil if comp move was not made' do
-      played_ttt = TictactoeBoard.new
-      played_ttt.board_arr = ["X", "O", "X",
-                              "O", "X", "O",
-                              "O", "X", "O"]
-      completed_game = Game.new({gametype: played_ttt, ui: ui, rules: rules, comp_player: comp_player})
-      expect(completed_game.comp_move).to eq nil
-    end
-  end
 
   # describe '#alternate_move' do
   #   it 'plays the first turn of the game' do
