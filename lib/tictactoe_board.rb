ecf8b35869a3f1c9cd06686ef54f6777e6ad3163
portatlas <a.vathanaka@gmail.com>
2017-01-03 11:49:25 +0700

    Change variable name to board_arr

--- lib/tictactoe_board.rb
+++ lib/tictactoe_board.rb
@@ -2,22 +2,22 @@ $: << File.dirname(__FILE__)
 
 class TictactoeBoard
 
-  attr_accessor :board, :turn
+  attr_accessor :board_arr, :turn
   attr_reader :valid_slots
 
-  def initialize (board = Array.new(9, " "), turn = "X")
-    @board = board
+  def initialize (board_arr = Array.new(9, " "), turn = "X")
+    @board_arr = board_arr
     @turn = turn
   end
 
   def move(input)
-      played_move = TictactoeBoard.new(@board.dup, whose_turn("O", "X"))
-      played_move.board[input.to_i] = turn
+      played_move = TictactoeBoard.new(@board_arr.dup, whose_turn("O", "X"))
+      played_move.board_arr[input.to_i] = turn
       played_move
   end
 
   def valid_slots
-    @board.each_with_index.map{|symbol, index| index if symbol==" "}.reject{|slots| slots == nil }
+    @board_arr.each_with_index.map{|symbol, index| index if symbol==" "}.reject{|slots| slots == nil }
   end
 
   def whose_turn(x, o)
