ed766f634166784733591bafce7ec4566cd698a8
portatlas <a.vathanaka@gmail.com>
2017-01-03 20:15:30 +0700

    Add comp_player to WebGameEngine

--- lib/web_game_engine.rb
+++ lib/web_game_engine.rb
@@ -3,9 +3,11 @@ $: << File.dirname(__FILE__)
 class WebGameEngine
 
   attr_reader :ttt_board
+  attr_accessor :comp_player
 
   def initialize(args)
     @ttt_board = args.fetch(:ttt_board, nil)
+    @comp_player = args.fetch(:comp_player, nil)
   end
 
 end
