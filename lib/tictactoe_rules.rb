51d072f145d5efd666395a85010d14ca1d28172b
portatlas <a.vathanaka@gmail.com>
2017-01-03 11:38:37 +0700

    Add game desc to rules

--- lib/tictactoe_rules.rb
+++ lib/tictactoe_rules.rb
@@ -1,8 +1,16 @@
 class TictactoeRules
 
+  attr_reader :desc
+
   WIN_COMBOS = [[0,1,2],[3,4,5],[6,7,8],
                 [0,3,6],[1,4,7],[2,5,8],
                 [0,4,8],[2,4,6]]
+
+  def initialize
+    @desc = {
+             name: "TacTacToe",
+     instructions: "You (X) and the computer (O) will take turns placing a 'X' and 'O' respectively, the player who succeeds in placing three of their marks in a horizontal, vertical or diagonal row wins."}
+  end
 
   def won?(board, turn)
     WIN_COMBOS.detect do |win_combo|
