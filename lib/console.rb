530b76f362484300f60689de5166b194af3d1445
portatlas <a.vathanaka@gmail.com>
2017-01-03 11:37:46 +0700

    Reference rules instead of board

--- lib/console.rb
+++ lib/console.rb
@@ -10,9 +10,9 @@ class Console
     puts " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
   end
 
-  def display_intro_msg(game_being_played)
-    puts "Welcome to #{game_being_played.desc[:name]}"
-    puts "#{game_being_played.desc[:instructions]}"
+  def display_intro_msg(game_being_played, rules)
+    puts "Welcome to #{rules.desc[:name]}"
+    puts "#{rules.desc[:instructions]}"
   end
 
   def prompt_user_for_input(game_being_played)
