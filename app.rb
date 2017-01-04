dd56b3f26e995777d0268ec723185c6596a9cdd7
portatlas <a.vathanaka@gmail.com>
2017-01-04 20:49:46 +0700

    Add methods two play user and comp

--- app.rb
+++ app.rb
@@ -21,17 +21,27 @@ class TicTacToe < Sinatra::Base
     erb :game
   end
 
+  post '/game' do
+    session[:game_mode] = params["mode"]
+    if session[:game_mode] == 'comp'
+      session[:player_2] = PlayerAi.new
+    elsif session[:game_mode] == 'player'
+      session[:player_2] = PlayerUser.new
+    end
+    redirect to ('/game')
+  end
+
   post '/game/move' do
     rules = TictactoeRules.new
     user_player_1 = PlayerUser.new
-    user_player_2 = PlayerUser.new
-    @game = WebGameEngine.new({ttt_board: session[:board], rules: rules, player_1: user_player_1, player_2: user_player_2})
+    @game = WebGameEngine.new({ttt_board: session[:board], rules: rules, player_1: user_player_1, player_2: session[:player_2]})
 
     @player_input = params[:grid_position].to_i
 
-    if session[:board].valid_move?(@player_input)
-      session[:board] = @game.player_1.user_move(session[:board], @player_input)
-      session[:board] = @game.ttt_board.move(@player_input)
+    if session[:game_mode] == 'player'
+      versus_user
+    elsif session[:game_mode] == 'comp'
+      versus_comp(rules)
     end
 
     if @game.rules.game_over?(session[:board], session[:board].turn)
@@ -46,6 +56,20 @@ class TicTacToe < Sinatra::Base
   get '/game/result' do
     erb :result
   end
+
+  def versus_user
+    if session[:board].valid_move?(@player_input)
+      session[:board] = @game.player_1.user_move(session[:board], @player_input)
+    end
+  end
+
+  def versus_comp(rules)
+    if session[:board].valid_move?(@player_input)
+      session[:board] = @game.player_1.user_move(session[:board], @player_input)
+      session[:board] = @game.player_2.ai_move(session[:board], rules)
+    end
+  end
+
 
 
 
