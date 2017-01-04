b382fef3a9ee9dfd37e5eee4d5384b8b1e6fa6d2
portatlas <a.vathanaka@gmail.com>
2017-01-04 13:58:34 +0700

    Clean up commented code

--- app.rb
+++ app.rb
@@ -17,23 +17,8 @@ class TicTacToe < Sinatra::Base
 
   get '/game' do
     session[:board] = TictactoeBoard.new
-    # rules = TictactoeRules.new
-    # comp_player = CompPlayer.new
-    # @game = WebGameEngine.new({ttt_board: ttt_board, rules: rules, comp_player: comp_player})
-    # session[:board] = @game.ttt_board
     erb :game
   end
-
-    # post '/game' do
-    #   ttt_board = TictactoeBoard.new
-    #   rules = TictactoeRules.new
-    #   comp_player = CompPlayer.new
-    #   @game = WebGameEngine.new({ttt_board: @ttt_board, rules: rules, comp_player: comp_player})
-    #   # session['game'] = @game.ttt_board
-
-
-    #   erb :game
-    # end
 
   post '/game/move' do
     rules = TictactoeRules.new
@@ -42,41 +27,9 @@ class TicTacToe < Sinatra::Base
 
     @player_input = params[:grid_position]
     session[:board] = @game.ttt_board.move(@player_input)
-    # @ttt_board = session[:game].comp_player.comp_move(@ttt_board, session[:game].rules)
+
     erb :game
   end
 
-  # it doesn't store it to sessions
-
-  # post '/game/move' do
-  #   @ttt_board = session[:board]
-  #   rules = TictactoeRules.new
-  #   comp_player = CompPlayer.new
-  #   @game = WebGameEngine.new(ttt_board: @ttt_board, rules: rules, comp_player: comp_player)
-  #   @player_input = params[:grid_position]
-  #   @ttt_board = @game.ttt_board.move(@player_input)
-  #   @ttt_board = @game.comp_player.comp_move(@ttt_board, rules)
-
-  #   # binding.pry
-
-  #   # redirect to ('/game')
-  #   erb :game
-  # end
-
-  # get '/game' do
-  #   create_new_game
-  #   erb :game
-  # end
-
-  def create_new_game
-    session[:game] = WebGameEngine.new({ttt_board: TictactoeBoard.new, rules: TictactoeRules.new, comp_player: CompPlayer.new})
-  end
-
-  def user_move
-
-  end
-
-  def comp_move
-  end
 
 end
