57921d72ff0400eda6674f340c189cd73826aef0
portatlas <a.vathanaka@gmail.com>
2017-01-04 22:06:07 +0700

    Refactor game engine initialization

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -5,6 +5,10 @@ class GameEngine
   attr_reader :ttt_board, :player_1, :player_2, :rules
 
   def initialize(args)
+    @ttt_board = args.fetch(:ttt_board, nil)
+    @rules = args.fetch(:rules, nil)
+    @player_1 = args.fetch(:player_1, nil)
+    @player_2 = args.fetch(:player_2, nil)
   end
 
 end
