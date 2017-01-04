5f5cab8db711494bbbc8171ea53b57764b7345dc
portatlas <a.vathanaka@gmail.com>
2017-01-04 15:49:03 +0700

    Change to player_1 and player_2 to be more flexible

--- lib/console_game_engine.rb
+++ lib/console_game_engine.rb
@@ -2,22 +2,22 @@ $: << File.dirname(__FILE__)
 
 class ConsoleGameEngine
 
-  attr_reader :gametype, :rules, :comp_player, :ui, :first_player
+  attr_reader :gametype, :rules, :player_1, :player_2, :ui
 
   def initialize(args)
     @gametype = args.fetch(:gametype, nil)
     @rules = args.fetch(:rules, nil)
-    @first_player = args.fetch(:first_player, nil)
-    @comp_player = args.fetch(:comp_player, nil)
+    @player_1 = args.fetch(:player_1, nil)
+    @player_2 = args.fetch(:player_2, nil)
     @ui = args.fetch(:ui, nil)
   end
 
   def alternate_move
     index_position = ui.get_user_input
     if @gametype.valid_move?(index_position)
-      @gametype = first_player.user_move(gametype, index_position)
+      @gametype = player_1.user_move(gametype, index_position)
       if !@gametype.valid_slots.empty?
-        @gametype = comp_player.ai_move(gametype, rules)
+        @gametype = player_2.ai_move(gametype, rules)
       end
       ui.show_board(@gametype.board_arr)
       ui.display_winner_message(rules, gametype)
