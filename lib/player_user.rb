7dd2f54a41368833d7db7bb6e3475a0401ddec98
portatlas <a.vathanaka@gmail.com>
2017-01-04 15:44:48 +0700

    Strip out checking logic from #user_move

--- lib/player_user.rb
+++ lib/player_user.rb
@@ -2,9 +2,8 @@ $: << File.dirname(__FILE__)
 
 class PlayerUser
 
-  def user_move(board, rules, user_input)
-    if board.valid_slots.include?(user_input)
-      board.move(user_input)
-    end
+  def user_move(board, user_input)
+    int_input = user_input.to_i
+    board.move(int_input)
   end
 end
