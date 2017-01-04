42f5aabda7b4833309fa88a01006b4f0fb05388c
portatlas <a.vathanaka@gmail.com>
2017-01-04 22:05:19 +0700

    Remove commented code

--- spec/console_game_engine_spec.rb
+++ spec/console_game_engine_spec.rb
@@ -6,30 +6,16 @@ require 'console'
 
 describe ConsoleGameEngine do
   let(:ttt_board){TictactoeBoard.new}
-  let(:io){Console.new}
+  let(:console){Console.new}
   let(:ttt_rules){TictactoeRules.new}
   let(:comp_player){PlayerAi.new}
-  let(:new_game){ConsoleGameEngine.new({ttt_board: ttt_board, rules: ttt_rules, comp_player: comp_player})}
+  let(:new_game){ConsoleGameEngine.new({ttt_board: ttt_board, rules: ttt_rules, comp_player: comp_player, ui: console})}
 
   describe ' #initialize' do
-    it 'with a new instance of TicTacToe' do
-      expect(new_game.ttt_board).to eq(ttt_board)
-    end
-    it 'with a new instance of TicTacToeRules' do
-      expect(new_game.rules).to eq(ttt_rules)
+    it 'with a new instance of Console' do
+      expect(new_game.ui).to eq(console)
     end
   end
-
-  # describe '#alternate_move' do
-  #   it 'plays the first turn of the game' do
-  #     new_game.alternate_move
-  #     board_after_first_move = new_game.ttt_board.board_arr
-
-  #     expect(board_after_first_move).to match_array(["X", " ", " ",
-  #                                                    " ", "O", " ",
-  #                                                    " ", " ", " "])
-  #   end
-  # end
 
   describe '#play' do
     it 'continues to play as long as the game is not over' do
