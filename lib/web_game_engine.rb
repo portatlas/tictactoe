f5911bf7b8baea71950230affe0603be87029d6a
portatlas <a.vathanaka@gmail.com>
2017-01-04 22:39:53 +0700

    Refactor to take out duplicate checking of validmove

--- lib/web_game_engine.rb
+++ lib/web_game_engine.rb
@@ -8,16 +8,12 @@ class WebGameEngine < GameEngine
   end
 
   def versus_user(current_board, player, user_input)
-    if current_board.valid_move?(user_input)
-      current_board = player.user_move(current_board, user_input)
-    end
+    current_board = player.user_move(current_board, user_input)
   end
 
   def versus_comp(current_board, rules, player_1, player_1_input, player_2)
-    if current_board.valid_move?(player_1_input)
       current_board = player_1.user_move(current_board, player_1_input)
       current_board = player_2.ai_move(current_board, rules)
-    end
   end
 
 end
